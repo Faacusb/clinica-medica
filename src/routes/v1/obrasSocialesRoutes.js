1fc3e23ce145a69f8f1c248bfe9d43cb47f624af
malenamoyano <malenamoyanov@gmail.com>
2026-06-14 12:11:02 -0300

    validacion obras sociales

--- src/routes/v1/obrasSocialesRoutes.js
+++ src/routes/v1/obrasSocialesRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { param } from "express-validator";
 import ObrasSocialesController from "../../controllers/obrasSocialesController.js";
 import { validarCampos, validarId } from "../../middleware/validationMiddleware.js";
-import { validarObraSocial } from "../../middleware/obrasSocialesValidation.js";
+import { validarObraSocial, validarActualizacionObraSocial } from "../../middleware/obrasSocialesValidation.js";
 
 const router = express.Router();
 const obrasSocialesController = new ObrasSocialesController();
@@ -10,7 +10,7 @@ const obrasSocialesController = new ObrasSocialesController();
 router.get("/", obrasSocialesController.listarObrasSociales);
 router.get("/:id", validarId,validarCampos, obrasSocialesController.obtenerPorId);
 router.post("/", validarObraSocial,validarCampos,obrasSocialesController.crearObraSocial);
-router.put("/:id",validarId, validarCampos,obrasSocialesController.editarObraSociales);
+router.put("/:id", validarId, validarActualizacionObraSocial, validarCampos,obrasSocialesController.editarObraSociales);
 router.delete("/:id",validarId,validarCampos,obrasSocialesController.eliminarObrasSociales);
 
 export default router;
